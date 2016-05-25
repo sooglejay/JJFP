a1feedca7fcf30b38770d800107302ec98ecf418
sooglejay <sooglejay@gmail.com>
2016-05-25 22:21:14 +0800

    fix  project

---
+++ app/src/main/java/jzfp/com/precisionpoverty/adapter/KindSpinnerAdapter.java
@@ -0,0 +1,64 @@
+package jzfp.com.precisionpoverty.adapter;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jzfp.com.precisionpoverty.R;
+
+/**
+ * Created by JammyQtheLab on 2015/12/16.
+ */
+public class KindSpinnerAdapter extends BaseAdapter {
+
+    private List<String> mDatas = new ArrayList<>();
+
+    public KindSpinnerAdapter(Activity activity, List<String> mDatas) {
+        this.activity = activity;
+        this.mDatas = mDatas;
+    }
+
+    private Activity activity;
+
+    @Override
+    public int getCount() {
+        return mDatas.size();
+    }
+
+    @Override
+    public String getItem(int i) {
+        return mDatas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder = new ViewHolder();
+        if (view == null) {
+            view = View.inflate(activity, R.layout.item_adapter_spinner_kind, null);
+            holder.tvName = (TextView) view.findViewById(R.id.tv_name);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        holder.tvName.setText(getItem(i));
+        return view;
+    }
+
+    private static class ViewHolder {
+        private TextView tvName;
+        private View.OnClickListener listener;
+
+    }
+}
--- app/src/main/java/jzfp/com/precisionpoverty/fragment/RequirementFragment.java
+++ app/src/main/java/jzfp/com/precisionpoverty/fragment/RequirementFragment.java
@@ -3,13 +3,22 @@ package jzfp.com.precisionpoverty.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-import jzfp.com.precisionpoverty.Constants.StringConstant;
+import java.util.ArrayList;
+import java.util.List;
+
 import jzfp.com.precisionpoverty.R;
+import jzfp.com.precisionpoverty.adapter.AreaSpinnerAdapter;
+import jzfp.com.precisionpoverty.adapter.KindSpinnerAdapter;
 import jzfp.com.precisionpoverty.adapter.RequirementAdapter;
 import jzfp.com.precisionpoverty.widgets.TitleBar;
 
@@ -21,6 +30,16 @@ public class RequirementFragment extends BaseFragment {
     private Activity context;
     private ListView listView;
     private RequirementAdapter adapter;
+
+
+    private View headerOfListView;
+    private Spinner spinnerArea;
+    private Spinner spinnerKind;
+    private KindSpinnerAdapter kindSpinnerAdapter;
+    private AreaSpinnerAdapter areaSpinnerAdapter;
+    private List<String> kindListDatas = new ArrayList<>();
+    private List<String> areaListDatas = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,14 +58,38 @@ public class RequirementFragment extends BaseFragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        setUp(view, savedInstanceState);
+        context = this.getActivity();
+        setUpView(view, savedInstanceState);
+        setDatas();
+        setUpListener();
     }
 
+    private void setDatas() {
+        kindListDatas.add("刷股");
+        kindListDatas.add("啊说");
+        kindListDatas.add("阿斯顿");
+        kindListDatas.add("叨逼叨");
+        kindListDatas.add("是的");
+        kindListDatas.add("阿萨");
+        kindListDatas.add("仿佛好");
+        kindListDatas.add("仿佛好");
 
-    private void setUp(View view, Bundle savedInstanceState) {
-        context = this.getActivity();
+        areaListDatas.add("刷股");
+        areaListDatas.add("啊说");
+        areaListDatas.add("阿斯顿");
+        areaListDatas.add("叨逼叨");
+        areaListDatas.add("是的");
+        areaListDatas.add("阿萨");
+        areaListDatas.add("仿佛好");
+        areaListDatas.add("仿佛好");
 
-        titleBar = (TitleBar) view.findViewById(R.id.title_bar);
+        kindSpinnerAdapter.notifyDataSetChanged();
+        areaSpinnerAdapter.notifyDataSetChanged();
+
+    }
+
+    private void setUpListener() {
+
         titleBar.setOnTitleBarClickListener(new TitleBar.OnTitleBarClickListener() {
             @Override
             public void onSearch(View v) {
@@ -58,10 +101,35 @@ public class RequirementFragment extends BaseFragment {
 
             }
         });
+        listView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float y = event.getY();
+                float x= event.getX();
+                Log.e("jwjw", event.getAction()+"   y="+y+"    x="+x);
+                return false;
+            }
+        });
+    }
 
+
+    private void setUpView(View view, Bundle savedInstanceState) {
+        titleBar = (TitleBar) view.findViewById(R.id.title_bar);
         listView = (ListView) view.findViewById(R.id.list_view);
+        headerOfListView = View.inflate(getActivity(), R.layout.header_of_list_view_in_requirement_fragment, null);
+
+        spinnerKind = (Spinner) headerOfListView.findViewById(R.id.spinner_kind);
+        spinnerArea = (Spinner) headerOfListView.findViewById(R.id.spinner_area);
+        listView.addHeaderView(headerOfListView);
+
         adapter = new RequirementAdapter(context);
+        kindSpinnerAdapter = new KindSpinnerAdapter(getActivity(), kindListDatas);
+        areaSpinnerAdapter = new AreaSpinnerAdapter(getActivity(), areaListDatas);
+
         listView.setAdapter(adapter);
+        spinnerArea.setAdapter(areaSpinnerAdapter);
+        spinnerKind.setAdapter(kindSpinnerAdapter);
+
     }
 
 
